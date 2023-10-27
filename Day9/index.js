3252108d7602f5931cec0e6c4d730032748669da
priyadarshani <priyarrsdce@gmail.com>
2023-10-27 21:43:29 +0530

    Question3

--- Day9/index.js
+++ Day9/index.js
@@ -26,3 +26,21 @@ function each(array, func) {
 each([1,2,3,4], function(val){
     console.log(val*2);
 });
+
+/*
+Question 3. Write a function called `map` which accepts two parameters: an array and a callback. 
+The `map` function should return a new array with the result of each value being passed to the callback function.
+*/
+
+function map(array, func) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      result.push(func(array[i]));
+    }
+    return result;
+  }
+  function double(n) {
+    return n * 2;
+  }
+  
+  console.log(map([1, 2, 3], double)); 
