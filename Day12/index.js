35a18ca9231ed4ce8583d3f21bb929ddac13adc8
priyadarshani <priyarrsdce@gmail.com>
2023-10-30 11:04:46 +0530

    adding

---
+++ Day12/index.js
@@ -0,0 +1,27 @@
+// **************** Functions in JS ************************
+
+// 1. Function to compute the product of two numbers
+
+// function product(a,b){
+// return (a*b);
+// };
+// let result = product(3,4);
+// console.log(result);
+
+//============================================================
+// Call a function which performs a calculation and returns the result:
+
+// let x = productFunction(4,3);
+// console.log(x);
+// function productFunction(a, b) {
+//     return a*b;
+// }
+
+// call a function that converts from Fahrenheit to Celsius:
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+  }
+  let value = toCelsius(77);
+  console.log(`The Celsius value is ${value}`);
+
