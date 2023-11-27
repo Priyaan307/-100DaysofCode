50b2ca6a37695885ae187204dfff39dc591bc1fe
priyadarshani <priyarrsdce@gmail.com>
2023-11-27 22:08:38 +0530

    Day40

---
+++ Day40/PostMan.md
@@ -0,0 +1,6 @@
+- Introduction to Postman and its basic Freatures
+
+- What is Postman?
+Postman is an API platform for building and using APIs.Postman simplifies each step of the API lifecycle and streamlines collaboration so we can create better APIs Faster. 
+
+- 10 Postman Features:
---
+++ Day40/stringManipulation_JS/index.js
@@ -0,0 +1,32 @@
+// String Manipulation:
+// Representation of String:
+
+// let str = "Hello";
+// let str1 = new String('Hello'); 
+// console.log(str); // Hello 
+// console.log(typeof str); // string
+// console.log(str1); // [String: 'Hello]
+// console.log(typeof str1); // object
+
+// Concatenation of Strings
+// concat string Manipulation:
+ let str2 = 'Hello2';
+ let str3 = "Hello3";
+ console.log(str2);
+ console.log(str3);
+ console.log(str2 + str3);
+ console.log(str2.concat(str3));
+
+// String interpolation, toLower, toUpper, length, indexOf()
+console.log(`First string: ${str2.toLowerCase()}`);
+
+//Bracket notation to access, charAt
+
+// +, Substring, Slice(subsequences)
+// difference between Substring and slice
+// trim, replace, include (trim removes the external spaces (removes both end white  spaces))
+
+//JSON.parse
+
+//split
+// spread operator
